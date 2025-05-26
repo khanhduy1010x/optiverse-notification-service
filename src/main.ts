619168132b075dd39202f3219aa989149c82c407
*I0BDDgOAjY <nguyenkhanhduy92000@gmail.com>
2025-05-27 00:57:02 +0700

    OPTV-27 feature: add middleware and change .env add path enviroment and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthMiddleware } from './midlleware/auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+   app.enableCors({
+    origin: true,
+    credentials: true, 
+  });
+  app.use(new AuthMiddleware().use);
+  await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
